1781c52b21349275cca49db432af0310054a0807
ChadCooper06 <chad.cooper@student.awesomeinc.org>
2022-03-23 01:42:26 +0000

    added some css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import LandingPage from './landing';
 
 function App() {
   return (
     <div className="app">
       <h1>My portfolio and blog</h1>
+      <LandingPage />
     </div>
   );
 }
--- src/landing.js
+++ src/landing.js
@@ -1,13 +1,11 @@
 import React from "react";
-
+import './App.css';
 export default function LandingPage() {
 
 return(
     <div className='landing'>
-        <div className='landing-text'
-        style={{
-            backgroundImage: 
-        }}>
+        <div className='landing-text'>
+            <h1>Portfolio</h1>
 
         </div>
     </div>
