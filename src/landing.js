d16fd8430e5f658b92efaf42f81281a85c487802
Chad Cooper <chad.cooper@student.awesomeinc.org>
2022-03-30 20:04:37 -0400

    Update landing.js

--- src/landing.js
+++ src/landing.js
@@ -1,11 +1,13 @@
 import React from "react";
-import './App.css';
+import "./App.css";
+import Layout from "./layout";
 
 
 const LandingPage() {
 
     return(
         <div className='landing'>
+            <Layout />
             <div className='landing-text'>
                 <h1>Chad Cooper</h1>
                 <h3>Full stack web developer</h3>
