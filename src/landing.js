d2191b1fad85c6bb3461487dc0f6a9b135f313d7
Chad Cooper <chad.cooper@student.awesomeinc.org>
2022-04-17 12:06:53 -0400

    Updating Landing with some bootstrap

--- src/landing.js
+++ src/landing.js
@@ -1,12 +1,15 @@
 import React from "react";
 import './App.css';
-export default function LandingPage() {
+//import "./LandingPage.css";
+//import button {Button} from "react-bootstrap";
 
+export default function LandingPage() {
 return(
     <div className='landing'>
         <div className='landing-text'>
             <h1>Portfolio</h1>
-
+            <p>Full Stack Web Developer</p>
+            <Button variant="warning">Meet Me</Button>
         </div>
     </div>
 )
