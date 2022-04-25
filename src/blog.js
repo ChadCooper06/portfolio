e95e0bad7007cc7f83fd541961f9ec999340b395
Chad Cooper <chad.cooper@student.awesomeinc.org>
2022-04-24 21:25:21 -0400

    Update blog.js
    Added the layout of the blog posts on the page so they show up as the format I decided on

--- src/blog.js
+++ src/blog.js
@@ -1,22 +1,37 @@
 // this is the layout for each post
 
-import React from 'react';
-import './App.css';
-import Posts from './posts';
+import React from "react";
+import "./blog.css";
+import { Tab, Tabs } from "react-bootstrap";
+import January22 from "./Blogs/January22";
+import February22 from "./Blogs/February22";
+import March22 from "./Blogs/March22";
+import April22 from "./Blogs/April22";
 
-//create the posts layout for the blog
-const BlogPosts = ({ posts, setPosts }) => {
-    return (
-        <div className="posts-container">
-            <div className="posts">
-               {posts.map((post) => (
-                   <Posts key={post.id} 
-                   post={post} 
-                   posts={posts} 
-                   setPosts={setPosts} />
-               ))}
-            </div>
-        </div>
-    ) 
+
+
+export default function Blog() {
+  return (
+    <div>
+      <h2>Blog</h2>
+      <Tabs
+        defaultActiveKey="april22"
+        id="uncontrolled-tab-example"
+        className="mb-3"
+      >
+        <Tab eventKey="april22" title="Most Recent">
+          <April22/>
+        </Tab>
+        <Tab eventKey="march22" title="March 2022">
+          <March22/>
+        </Tab>
+        <Tab eventKey="feb22" title="February 2022">
+          <February22/>
+        </Tab>
+        <Tab eventKey="jan22" title="January 2022">
+          <January22/>
+        </Tab>
+      </Tabs>
+    </div>
+  );
 }
-export default BlogPosts
