6f9f55f542c2a5c2bfe98391188cc33e147b8b69
Chad Cooper <chad.cooper@student.awesomeinc.org>
2022-06-15 18:32:29 -0400

    Create about.js

---
+++ src/about.js
@@ -0,0 +1,28 @@
+import React from 'react';
+//import aboutImage from '../pictures/surfing.jpeg'
+
+export default function AboutMe() {
+  return (
+    <div className="aboutPageContainer" >
+      <div className="aboutImageWrapper">
+        <img className="aboutImage" src={aboutImage} alt="about me"></img>
+      </div>
+        <h1 className="aboutPageHeader">About me.</h1>
+      <div className="aboutMeBioBox">
+        <p className="aboutMeBio">
+          Hey there! I'm Chad and I'm a full stack Jr developer ready to make my mark. From January to May 2022 I attended and completed an 
+          intensive 16 week web development bootcamp through Awesome Inc. This taught me how to learn code as well as multiple languages, 
+          frameworks and libraries. The decision to attend the bootcamp was a hard one but was also one of my best decisions. I loved every moment 
+          and I am more than ready to continue my journey as a developer.<br/>
+          <br/>
+          I enjoy learning, sharing what I know, and breaking things down to see how they work. Writing code has been an amazing way for me to 
+          do all of those at the same time and I'm only sad I didn't start sooner. Prior to attending the bootcamp I have had multiple positions 
+          in hospitals, manufacturing and governmental work but none ever left me loving what I was doing. Writing code has changed that for me.<br/>
+          <br/>
+          Outside of coding, I enjoy; reading, cooking, hiking/camping, learning new things (I taught myself multiple edible and medicinal wild plants as 
+          well as how to survive in the wilderness), working out and being a chew toy for my cat.
+        </p>
+      </div>
+    </div>
+  )
+}
