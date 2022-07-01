504053fe4b37954d9bd59778e819f92a6cc29957
ChadCooper06 <chadcooper06@gmail.com>
2022-07-01 16:43:22 +0000

    small chnage to icons

--- src/App.js
+++ src/App.js
@@ -16,9 +16,9 @@ function App() {
           <Link className="nav-link" to="/blog">BLOG</Link>
         </div> 
         <div className="iconBlock">
-          <Link className="icon" onPress={() => useHref.openURL("mailto:chadcooper06@gmail.com")} width="60px" height="60px" alt="email" target="_blank"><GoMail /></Link>
-          <Link className="icon" onPress={() => useHref.openURL("https://github.com/chadcooper06")} width="60px" height="60px" alt="github" target="_blank"><GoMarkGithub /></Link>
-          <Link className="icon" onPress={() => useHref.openURL("https://www.linkedin.com/in/chad-cooper-dev/")} width="60px" height="60px" alt="call me" target="_blank"><GrLinkedin /></Link> 
+          <Link className="icon" to={useHref.openURL("mailto:chadcooper06@gmail.com")} width="60px" height="60px" alt="email" target="_blank"><GoMail /></Link>
+          <Link className="icon" to={useHref.openURL("https://github.com/chadcooper06")} width="60px" height="60px" alt="github" target="_blank"><GoMarkGithub /></Link>
+          <Link className="icon" to={useHref.openURL("https://www.linkedin.com/in/chad-cooper-dev/")} width="60px" height="60px" alt="call me" target="_blank"><GrLinkedin /></Link> 
         </div>
       </div>
   <Outlet />
